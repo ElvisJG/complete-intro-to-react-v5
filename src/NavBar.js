ff8b0e5c0b2a86ba67c301374f7c46fad205d5ee
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-11 18:49:52 -0400

    Code-Splitting and Lazy Loading

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
+import colors from "./colors";
+
+const spin = keyframes`
+    to  {
+        transform: rotate(360deg);
+    }
+`;
 
 const NavBar = () => {
   return (
     <header
       css={css`
-        background-color: #333;
+        background-color: ${colors.dark};
         padding: 15px;
+
+        &:hover {
+          text-decoration: underline;
+        }
       `}
     >
       <Link to="/">Adopt Me!</Link>
       <span
         css={css`
           font-size: 60px;
+          display: inline-block;
+          animation: 1s ${spin} linear infinite;
         `}
         role="img"
         aria-label="logo"
